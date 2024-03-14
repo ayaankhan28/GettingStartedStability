e526f13ae95d492496b878eac5d38b2f3e500c1c
ayaankhan28 <98885614+ayaankhan28@users.noreply.github.com>
2024-03-15 03:38:34 +0530

    Update Text_To_Image.py

--- Text_To_Image.py
+++ Text_To_Image.py
@@ -2,22 +2,23 @@ import base64
 import requests
 import os
 from PIL import Image
+
 url = "https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image"
 
 body = {
-  "steps": 50,
-  "width": 1024,
-  "height": 1024,
+  "steps": 50,#Number of steps,More steps->better image->more time
+  "width": 1024,#widht of image
+  "height": 1024,#height of image
   "seed": 0,
   "cfg_scale": 5,
   "samples": 1,
   "text_prompts": [
     {
-      "text": "YOUR",
+      "text": "PROMPT",#Write your prompt here
       "weight": 1
     },
     {
-      "text": "blurry, bad",
+      "text": "blurry, bad",#Write keywords which needs to be excluded from output.
       "weight": -1
     }
   ],
@@ -26,7 +27,7 @@ body = {
 headers = {
   "Accept": "application/json",
   "Content-Type": "application/json",
-  "Authorization": "888",
+  "Authorization": "YOUR_API_KEY",
 }
 
 response = requests.post(
@@ -40,14 +41,12 @@ if response.status_code != 200:
 
 data = response.json()
 
-# make sure the out directory exists
-if not os.path.exists("./out"):
-    os.makedirs("./out")
+
 
 for i, image in enumerate(data["artifacts"]):
-    with open(f'./out/img.png', "wb") as f:
+    with open(f'.{"PATH TO YOU IMAGE"}/img.png', "wb") as f:
         f.write(base64.b64decode(image["base64"]))
 
 
-img = Image.open("./out/img.png")
+img = Image.open("./{"PATH TO YOU IMAGE"}/img.png")
 img.show()
